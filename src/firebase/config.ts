456b1a7a5d3220ca9ada84530bb94d6fc6ce06d1
CarlosAguilar <c82556691@gmail.com>
2025-10-20 12:45:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9989922705-e16ac",
+  "appId": "1:957066797877:web:bffcfff80fea0f90d1627c",
+  "apiKey": "AIzaSyAgHTAHViU720l-ikxl5nFxQuyy25GgUMo",
+  "authDomain": "studio-9989922705-e16ac.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "957066797877"
+};
